8ed3c5d676366bc8f28fa87ead93963489a89145
Stepan Koltsov <stepan.koltsov@gmail.com>
2024-05-27 22:28:15 +0100

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Help of command.
+///
+/// When this line is added, each option is printed multiline.
+#[derive(clap::Parser)]
+struct MyOpts {
+    /// Help of foo.
+    #[clap(long)]
+    foo: String,
+    /// Help of bar.
+    #[clap(long)]
+    bar: String,
+}
+
 fn main() {
+    MyOpts::parse();
     println!("Hello, world!");
 }
